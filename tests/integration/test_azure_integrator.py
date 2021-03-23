2445e923900d149a445bee44af0b9f0ac486f729
Cory Johns <johnsca@gmail.com>
2021-03-23 12:04:32 -0400

    Clean up integration test, test connectivity from units
    There was a lot of duplication between the integration tests, so I
    cleaned it up by using parameterization. Also addressed the review
    comment about the try / else, and added a connectivity check from the
    units (which is currently failing for self-access on the internal LB).

--- tests/integration/test_azure_integrator.py
+++ tests/integration/test_azure_integrator.py
@@ -1,5 +1,10 @@
+import logging
+
 import pytest
 import requests
+
+
+log = logging.getLogger(__name__)
 
 
 @pytest.mark.abort_on_fail
@@ -9,46 +14,11 @@ async def test_build_and_deploy(ops_test, lb_charms):
         pytest.skip("Already deployed")
     await ops_test.model.deploy(await ops_test.build_charm("."), trust=True)
     await ops_test.model.deploy(await ops_test.build_charm(lb_charms.lb_consumer))
-    await ops_test.model.wait_for_idle(timeout=20*60)
+    await ops_test.model.wait_for_idle(timeout=20 * 60)
 
 
-async def test_public(ops_test, list_lbs, list_public_ips, list_nsg_rules):
-    lb_consumer = ops_test.model.applications["lb-consumer"]
-    lb_unit = lb_consumer.units[0]
-    # Sanity check
-    assert await list_lbs() == []
-    assert await list_public_ips() == []
-    assert await list_nsg_rules() == []
-    assert lb_unit.workload_status == "waiting"
-    # Create public LB
-    await lb_consumer.set_config({"public": "true"})
-    await ops_test.model.add_relation("azure-integrator", "lb-consumer")
-    await ops_test.model.wait_for_idle()
-    try:
-        try:
-            # Check public LB creation
-            assert await list_lbs() != []
-            assert await list_public_ips() != []
-            assert await list_nsg_rules() != []
-            assert lb_unit.workload_status == "active"
-            address = lb_unit.workload_status_message
-            r = requests.get(f"http://{address}/")
-            assert r.status_code == 200
-            assert "nginx" in r.text.lower()
-        finally:
-            # Cleanup
-            await lb_consumer.remove_relation("lb-provider", "azure-integrator")
-            await ops_test.model.wait_for_idle()
-    except Exception:
-        raise
-    else:
-        # Check cleanup
-        assert await list_lbs() == []
-        assert await list_public_ips() == []
-        assert await list_nsg_rules() == []
-
-
-async def test_private(ops_test, list_lbs, list_public_ips, list_nsg_rules):
+@pytest.mark.parametrize("visibility", ["public", "internal"])
+async def test_lb(ops_test, list_lbs, list_public_ips, list_nsg_rules, visibility):
     lb_consumer = ops_test.model.applications["lb-consumer"]
     lb_unit = lb_consumer.units[0]
     az_unit = ops_test.model.applications["azure-integrator"].units[0]
@@ -57,28 +27,54 @@ async def test_private(ops_test, list_lbs, list_public_ips, list_nsg_rules):
     assert await list_public_ips() == []
     assert await list_nsg_rules() == []
     assert lb_unit.workload_status == "waiting"
-    # Create private LB
-    await lb_consumer.set_config({"public": "false"})
+    log.info(f"Creating {visibility} LB")
+    await lb_consumer.set_config(
+        {"public": "true" if visibility == "public" else "false"}
+    )
     await ops_test.model.add_relation("azure-integrator", "lb-consumer")
+    log.info("Waiting for LB")
     await ops_test.model.wait_for_idle()
+    is_failed = False
     try:
-        try:
-            # Check private LB creation
-            assert await list_lbs() != []
+        log.info("Verifying LB components")
+        assert await list_lbs() != []
+        if visibility == "public":
+            assert await list_public_ips() != []
+            assert await list_nsg_rules() != []
+        else:
             assert await list_public_ips() == []
             assert await list_nsg_rules() == []
-            assert lb_unit.workload_status == "active"
-            address = lb_unit.workload_status_message
-            data = await az_unit.run(f"curl -i 'http://{address}/'")
-            assert "nginx" in data.results["Stdout"]
-        finally:
-            # Cleanup
-            await lb_consumer.remove_relation("lb-provider", "azure-integrator")
-            await ops_test.model.wait_for_idle()
-    except Exception:
+        assert lb_unit.workload_status == "active"
+        address = lb_unit.workload_status_message
+        lb_url = f"http://{address}/"
+        if visibility == "public":
+            log.info(f"Confirming external access to {lb_url}")
+            r = requests.get(lb_url)
+            assert r.status_code == 200
+            assert "nginx" in r.text.lower()
+        for unit in (az_unit, lb_unit):
+            log.info(f"Confirming access from {unit.name} to {lb_url}")
+            data = await unit.run(f"curl -i '{lb_url}'")
+            output = data.results.get("Stdout", data.results.get("Stderr", ""))
+            assert "nginx" in output
+    except Exception as e:
+        is_failed = True
+        log.error(f"Failed: {e}")
         raise
-    else:
-        # Check cleanup
-        assert await list_lbs() == []
-        assert await list_public_ips() == []
-        assert await list_nsg_rules() == []
+    finally:
+        log.info("Cleaning up LB")
+        await lb_consumer.remove_relation("lb-provider", "azure-integrator")
+        await ops_test.model.wait_for_idle()
+        for check, dsc in [
+            (await list_lbs(), "LBs"),
+            (await list_public_ips(), "public IPs"),
+            (await list_nsg_rules(), "NSG rules"),
+        ]:
+            msg = f"Failed to clean up {dsc}: {check}"
+            if is_failed:
+                if check != []:
+                    # Only log failed cleanup, rather than assert, so as to not
+                    # mask other failure.
+                    log.error(msg)
+            else:
+                assert check == [], msg
